2b4a62796f2107f2adc9cf9e2a863270200b97dc
Christophe Liu <lsmgeb89@gmail.com>
2016-06-13 00:43:30 -0500

    [CS 6360][project_1] Implement loading data in sql script and upload raw data files

--- create_database_import_data.sql
+++ create_database_import_data.sql
@@ -1,22 +1,26 @@
+### Create Database ###
 DROP DATABASE IF EXISTS Library;
 CREATE DATABASE Library;
-
 USE Library;
 
+### Create Tables ###
 CREATE TABLE BOOK (
-  Isbn        CHAR(13)    NOT NULL    DEFAULT '0000000000000',
-  Title       VARCHAR(64) NOT NULL,
-  CONSTRAINT  pk_book     PRIMARY KEY (Isbn)
+  Book_id     CHAR(10)        NOT NULL,
+  Isbn        CHAR(13)        NOT NULL    DEFAULT '0000000000000',
+  Title       VARCHAR(256)    NOT NULL,
+  CONSTRAINT  pk_book         PRIMARY KEY (Isbn),
+  CONSTRAINT  un_book_book_id UNIQUE (Book_id)
 );
 
 CREATE TABLE AUTHORS (
-  Author_id   CHAR(9)     NOT NULL    DEFAULT '000000000',
-  Name        VARCHAR(64) NOT NULL,
-  CONSTRAINT  pk_authors  PRIMARY KEY (Author_id)
+  Author_id   INT UNSIGNED    NOT NULL    AUTO_INCREMENT,
+  Name        VARCHAR(64)     NOT NULL,
+  CONSTRAINT  pk_authors      PRIMARY KEY (Author_id),
+  CONSTRAINT  un_authors_name UNIQUE (Name)
 );
 
 CREATE TABLE BOOK_AUTHORS (
-  Author_id   CHAR(9)                   NOT NULL    DEFAULT '000000000',
+  Author_id   INT UNSIGNED              NOT NULL,
   Isbn        CHAR(13)                  NOT NULL    DEFAULT '0000000000000',
   CONSTRAINT  pk_book_authors           PRIMARY KEY (Author_id, Isbn),
   CONSTRAINT  kf_book_authors_author_id FOREIGN KEY (Author_id) REFERENCES AUTHORS(Author_id)
@@ -26,30 +30,33 @@ CREATE TABLE BOOK_AUTHORS (
 );
 
 CREATE TABLE LIBRARY_BRANCH (
-  Branch_id   VARCHAR(32)       NOT NULL,
-  Branch_name VARCHAR(32)       NOT NULL,
-  Address     VARCHAR(64)       NOT NULL,
-  CONSTRAINT  pk_library_branch PRIMARY KEY (Branch_id)
+  Branch_id     TINYINT(1) UNSIGNED NOT NULL,
+  Branch_name   VARCHAR(32)         NOT NULL,
+  Address       VARCHAR(64)         NOT NULL,
+  CONSTRAINT    pk_library_branch   PRIMARY KEY (Branch_id)
 );
 
 CREATE TABLE BOOK_COPIES (
-  Book_id     CHAR(10)                  NOT NULL,
-  Isbn        CHAR(13)                  NOT NULL,
-  Branch_id   VARCHAR(32)               NOT NULL,
-  CONSTRAINT  pk_book_copies            PRIMARY KEY (Book_id),
-  CONSTRAINT  pk_book_copies_isbn       FOREIGN KEY (Isbn)      REFERENCES BOOK(Isbn)
-              ON UPDATE CASCADE,
-  CONSTRAINT  pk_book_copies_branch_id  FOREIGN KEY (Branch_id) REFERENCES LIBRARY_BRANCH(Branch_id)
-              ON UPDATE CASCADE
+  Book_id       CHAR(10)                  NOT NULL,
+  Isbn          CHAR(13),
+  Branch_id     TINYINT(1) UNSIGNED       NOT NULL,
+  No_of_Copies  INT UNSIGNED              NOT NULL,                    
+  CONSTRAINT    pk_book_copies            PRIMARY KEY (Book_id, Branch_id),
+  CONSTRAINT    pk_book_copies_isbn       FOREIGN KEY (Isbn)      REFERENCES BOOK(Isbn)
+                ON UPDATE CASCADE,
+  CONSTRAINT    pk_book_copies_branch_id  FOREIGN KEY (Branch_id) REFERENCES LIBRARY_BRANCH(Branch_id)
+                ON UPDATE CASCADE
 );
 
 CREATE TABLE BORROWER (
   Card_no     CHAR(8)     NOT NULL    DEFAULT '00000000',
-  Ssn         CHAR(10)    NOT NULL,
+  Ssn         CHAR(11)    NOT NULL,
   Fname       VARCHAR(16) NOT NULL,
   Lname       VARCHAR(16) NOT NULL,
   Address     VARCHAR(64) NOT NULL,
-  Phone       CHAR(10)    NOT NULL,
+  City        VARCHAR(16) NOT NULL,
+  State       VARCHAR(16) NOT NULL,
+  Phone       CHAR(14)    NOT NULL,
   CONSTRAINT  pk_borrower PRIMARY KEY (Card_no)
 );
 
@@ -75,3 +82,68 @@ CREATE TABLE FINES (
   CONSTRAINT  fk_fines_load_id      FOREIGN KEY (Loan_id) REFERENCES BOOK_LOANS(Loan_id)
               ON UPDATE CASCADE
 );
+
+### Import Data ###
+LOAD DATA LOCAL INFILE 'C:/Users/ThinkPad-E531/Documents/Assignments/2016/Summer/CS 6360.0U1 - Database Design/project_1/data_source/library_branch.csv'
+     INTO TABLE LIBRARY_BRANCH
+   FIELDS TERMINATED BY '\t'
+ ENCLOSED BY ''
+    LINES TERMINATED BY '\n'
+   IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:/Users/ThinkPad-E531/Documents/Assignments/2016/Summer/CS 6360.0U1 - Database Design/project_1/data_source/borrowers.csv'
+     INTO TABLE BORROWER
+   FIELDS TERMINATED BY ','
+ ENCLOSED BY ''
+    LINES TERMINATED BY '\r\n'
+   IGNORE 1 ROWS
+   (Card_no, Ssn, Fname, Lname, @dummy, Address, City, State, Phone);
+
+LOAD DATA LOCAL INFILE 'C:/Users/ThinkPad-E531/Documents/Assignments/2016/Summer/CS 6360.0U1 - Database Design/project_1/data_source/books.csv'
+     INTO TABLE BOOK
+   FIELDS TERMINATED BY '\t'
+ ENCLOSED BY ''
+    LINES TERMINATED BY '\r\n'
+   IGNORE 1 ROWS
+   (Book_id, Isbn, Title, @dummy, @dummy, @dummy, @dummy);
+
+LOAD DATA LOCAL INFILE 'C:/Users/ThinkPad-E531/Documents/Assignments/2016/Summer/CS 6360.0U1 - Database Design/project_1/data_source/book_copies.csv'
+     INTO TABLE BOOK_COPIES
+   FIELDS TERMINATED BY '\t'
+ ENCLOSED BY ''
+    LINES TERMINATED BY '\r\n'
+   IGNORE 1 ROWS
+   (Book_id, Branch_id, No_of_Copies);
+
+UPDATE BOOK_COPIES T2
+JOIN  BOOK T1 ON T1.Book_id = T2.Book_id
+SET T2.Isbn = T1.Isbn;
+
+LOAD DATA LOCAL INFILE 'C:/Users/ThinkPad-E531/Documents/Assignments/2016/Summer/CS 6360.0U1 - Database Design/project_1/data_source/books.csv'
+     INTO TABLE AUTHORS
+   FIELDS TERMINATED BY '\t'
+ ENCLOSED BY ''
+    LINES TERMINATED BY '\r\n'
+   IGNORE 1 ROWS
+   (@dummy, @dummy, @dummy, Name, @dummy, @dummy, @dummy);
+
+LOAD DATA LOCAL INFILE 'C:/Users/ThinkPad-E531/Documents/Assignments/2016/Summer/CS 6360.0U1 - Database Design/project_1/data_source/books.csv'
+     INTO TABLE BOOK_AUTHORS
+   FIELDS TERMINATED BY '\t'
+ ENCLOSED BY ''
+    LINES TERMINATED BY '\r\n'
+   IGNORE 1 ROWS
+   (@dummy, Isbn, @dummy, @name, @dummy, @dummy, @dummy)
+      SET Author_id = (SELECT Author_id
+                      FROM AUTHORS
+                      WHERE Name = @name);
+
+### Show Data ###
+SELECT * FROM BOOK LIMIT 10;
+SELECT * FROM AUTHORS LIMIT 10;
+SELECT * FROM BOOK_AUTHORS LIMIT 10;
+SELECT * FROM LIBRARY_BRANCH LIMIT 10;
+SELECT * FROM BOOK_COPIES LIMIT 10;
+SELECT * FROM BORROWER LIMIT 10;
+SELECT * FROM BOOK_LOANS LIMIT 10;
+SELECT * FROM FINES LIMIT 10;
